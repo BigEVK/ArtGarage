51eb395fe48769134dbb29794b1a6f0e5568f80d
Eric VanKirk <eric@ericvankirk.com>
2022-08-31 22:32:07 -0500

    edit post

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,1 +1,25 @@
-;
+async function editFormHandler(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value.trim();
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length -1
+    ];
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            title
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
