97f3b0571dbf138cee62ab0a400e92139c154a68
Guadalupe Rocha <guadaluperocha@GuadalupesMBP2.attlocal.net>
2022-08-31 20:08:33 -0500

    connection js

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,3 @@
-;
+DROP DATABASE IF EXISTS art_garage_db;
+
+CREATE DATABASE art_garage_db;
--- models/User.js
+++ models/User.js
@@ -1,1 +1,49 @@
-;
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [4]
+        }
+    }
+}, {
+    hooks: {
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+        }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+})
+
+
+module.exports = User;
