aab586fa9852ecb23d31f9eadd95a86ed075bea8
Eric VanKirk <eric@ericvankirk.com>
2022-08-31 20:30:45 -0500

    add post js

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,5 +1,25 @@
 async function newFormHandler(event) {
     event.preventDefault();
 
-    const title = document.querySelector('input[name="post-title"]').value
+    const title = document.querySelector('input[name="post-title"]').value;
+    const post_url = document.querySelector('input[name="post-url"]').value;
+
+    const reposnse = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({
+            title, 
+            post_url
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
 }
+
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
