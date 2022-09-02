274e4c8b9ddcb4b2e295a9116979b95ee7954c29
Guadalupe Rocha <guadaluperocha@GuadalupesMBP2.attlocal.net>
2022-09-02 15:24:56 -0500

    handlebars controllers js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,1 +1,36 @@
-;
+const router = require('express').Router();
+const { Blog, User, Comment } = require('../../models');
+// get all blogs
+router.get('/', (req, res) => {
+    console.log('======================');
+    Blog.findAll({
+        attributes: [
+            'id',
+            'user_id',
+            'title',
+            'content',
+            'price',
+            'image_urls',
+            'created_at',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,55 @@
-;
+// title and body
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+// create our Post model
+class Post extends Model {
+
+}
+
+// create fields/columns for Post model
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        image_urls: {
+            type: [DataTypes.STRING],
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW
+        }
+
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
