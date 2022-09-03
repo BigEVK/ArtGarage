6425785f05ee3187f53e3104fa0421201eaa54c9
Eric VanKirk <eric@ericvankirk.com>
2022-09-03 12:34:17 -0500

    api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,5 @@
-;
+const router = require('express').Router();
+
+const userRoutes = require('./user-routes.js');
+router.use('/users', userRoutes);
+module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,1 +1,71 @@
-;
+const router = require('express').Router();
+const { User } = require('../../models')
+
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] },
+        where: { type: req.body.type }
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: { id: req.params.id }
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.post('/login', (req, res) => {
+
+    User.findOne({ where: req.body.username })
+        .then(dbUserData => {
+            if (User.checkPassword(req.body.password)) {
+                req.session.save(() => {
+                    req.session.user_id = dbUserData.id;
+                    req.session.username = dbUserData.username;
+                    req.session.loggedIn = true;
+
+                    res.redirect('/')
+                })
+            } else {
+                res.status(500).send('invalid password');
+            }
+
+        }).catch(err => {
+            res.status(500).send(err.message);
+        })
+});
+
+router.post('/signup', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
