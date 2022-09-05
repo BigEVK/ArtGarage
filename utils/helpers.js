879fb80ce62fcb6b579d2ca94b70579db9faa44d
bjennings90 <101131665+bjennings90@users.noreply.github.com>
2022-09-05 17:01:45 -0500

    added utils files

---
+++ utils/helpers.js
@@ -0,0 +1,10 @@
+function formatDate(date) {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+        date
+    ).getFullYear()}`;
+}
+
+
+module.exports = {
+    formatDate
+}
