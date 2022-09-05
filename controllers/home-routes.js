4e6c952434e4af5d845946955266aa71f0386ecf
Ron Dronet <ron@dronetdevdesign.com>
2022-09-05 14:09:30 -0500

    revised html and css -- added code for image upload

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,43 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 
+router.get('/', (req, res) => {
+  // console.log(req.session);
+  // Post.findAll({
+  //   limit: 5,
+  //   order: [['created_at', 'DESC']],
+  //   attributes: [
+  //     'id',
+  //     'title',
+  //     'created_at',
+  //     'content'
+  //   ],
+  //   include: [
+  //     {
+  //       model: Comment,
+  //       attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+  //       include: {
+  //         model: User,
+  //         attributes: ['username']
+  //       }
+  //     },
+  //     {
+  //       model: User,
+  //       attributes: ['username']
+  //     }
+  //   ]
+  // })
+  //   .then(dbPostData => {
+  //     const posts = dbPostData.map(post => post.get({ plain: true }));
+      // pass a single post object into the homepage template
+      res.render('homepage', {  });
+    // })
+    // .catch(err => {
+    //   console.log(err);
+    //   res.status(500).json(err);
+    // });
+});
+
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
@@ -12,8 +49,32 @@ router.get('/login', (req, res) => {
 router.get('/logout', (req, res) => {
   req.session.destroy((err) => {
     req.session = null;
-    res.redirect('/login');
+    res.redirect('/');
   })
 })
+
+// SIGNUP route:
+router.get('/signup', (req, res) => {
+  const{type: user_type} = req.query;
+  res.render('signup', {user_type: user_type || 'buyer', isArtist: user_type === 'artist'});
+});
+
+router.get('/profile/:id', (req, res) => {
+  const {id} = req.params;
+  res.render('artist-profile-page');
+});
+
+router.get('/update-profile', (req, res) => {
+  const { id } = req.params;
+  res.render('artist-update-page', {id});
+});
+
+router.get('/add-comments', (req, res) => {
+  res.render('single-post');
+});
+
+router.get('/artists', (req, res) => {
+  res.render('dashboard');
+});
 
 module.exports = router;
