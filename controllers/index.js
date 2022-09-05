c899acd961283783bbf9af142db36445830dfbab
bjennings90 <101131665+bjennings90@users.noreply.github.com>
2022-09-05 17:46:00 -0500

    updated images and javascript folders'

--- controllers/index.js
+++ controllers/index.js
@@ -7,4 +7,8 @@ router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
 
+router.use((req, res) => {
+    res.status(404).end();
+});
+
 module.exports = router;
