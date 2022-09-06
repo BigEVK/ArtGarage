80bc23981682914c46bd122e7b59da9f7e0a1ef6
Eric VanKirk <eric@ericvankirk.com>
2022-09-05 20:58:50 -0500

    latest update that works

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models')
+const withAuth = require('../../utils/auth');
+
 
 router.get('/', (req, res) => {
   User.findAll({
--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -4,7 +4,7 @@ async function newFormHandler(event) {
     const title = document.querySelector('input[name="post-title"]').value;
     const post_url = document.querySelector('input[name="post-url"]').value;
   
-    const reposnse = await fetch(`/api/posts`, {
+    const response = await fetch(`/api/posts`, {
       method: 'POST',
       body: JSON.stringify({
         title,
